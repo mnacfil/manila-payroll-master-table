cec3e8d3d99d05a53e962f85684b0460d78aad45
Melvin Nacfil <mnacfil@gmail.com>
2025-05-11 17:59:43 +0800

    make table reusable

--- frontend/src/components/features/employees/EmployeesContent.tsx
+++ frontend/src/components/features/employees/EmployeesContent.tsx
@@ -1,9 +1,75 @@
 import Table from "@/components/ui/table";
+import { ColumnProps } from "primereact/column";
+
+const employees = [
+  {
+    name: "Melvin",
+    code: "123",
+    category: "cst",
+    quantity: 123,
+  },
+  {
+    name: "John",
+    code: "23131312",
+    category: "cot",
+    quantity: 1,
+  },
+  {
+    name: "Cal",
+    code: "132asd",
+    category: "MST",
+    quantity: 44,
+  },
+  {
+    name: "Sen",
+    code: "ada123123",
+    category: "people",
+    quantity: 69,
+  },
+  {
+    name: "Jor",
+    code: "dasd23232",
+    category: "cst",
+    quantity: 22,
+  },
+];
+
+const columns: ColumnProps[] = [
+  {
+    field: "name",
+    header: "Name",
+  },
+  {
+    field: "code",
+    header: "Code",
+  },
+  {
+    field: "category",
+    header: "Category",
+  },
+  {
+    field: "quantity",
+    header: "Quantity",
+  },
+  {
+    header: "Action",
+    align: "center",
+    body: (rowData) => {
+      return (
+        <i
+          className="pi pi-ellipsis-h cursor-pointer"
+          aria-label="more-action"
+          role="button"
+        ></i>
+      );
+    },
+  },
+];
 
 const EmployeesContent = () => {
   return (
     <div>
-      <Table />
+      <Table data={employees} columns={columns} />
     </div>
   );
 };
--- frontend/src/components/ui/table.tsx
+++ frontend/src/components/ui/table.tsx
@@ -1,62 +1,21 @@
-import { Button } from "primereact/button";
-import { Column } from "primereact/column";
+import { Column, ColumnProps } from "primereact/column";
 import { DataTable } from "primereact/datatable";
 
-type Props = {};
+type Props = {
+  data: any[];
+  columns: ColumnProps[];
+};
 
-const products = [
-  {
-    name: "Melvin",
-    code: "123",
-    category: "cst",
-    quantity: 123,
-  },
-  {
-    name: "John",
-    code: "23131312",
-    category: "cot",
-    quantity: 1,
-  },
-  {
-    name: "Cal",
-    code: "132asd",
-    category: "MST",
-    quantity: 44,
-  },
-  {
-    name: "Sen",
-    code: "ada123123",
-    category: "people",
-    quantity: 69,
-  },
-  {
-    name: "Jor",
-    code: "dasd23232",
-    category: "cst",
-    quantity: 22,
-  },
-];
-
-const Table = () => {
+const Table = ({ columns, data }: Props) => {
   return (
-    <DataTable value={products} showGridlines>
-      <Column field="code" header="Code"></Column>
-      <Column field="name" header="Name"></Column>
-      <Column field="category" header="Category"></Column>
-      <Column field="quantity" header="Quantity"></Column>
-      <Column
-        header="Action"
-        align={"center"}
-        body={(rowData) => {
-          return (
-            <i
-              className="pi pi-ellipsis-h cursor-pointer"
-              aria-label="more-action"
-              role="button"
-            ></i>
-          );
-        }}
-      ></Column>
+    <DataTable value={data} showGridlines>
+      {columns.map((item) => (
+        <Column
+          field={item.field}
+          header={item.header}
+          body={item.body}
+        ></Column>
+      ))}
     </DataTable>
   );
 };
