6e31d931396392c9a0238f8949a5b0307a8200fa
Melvin Nacfil <mnacfil@gmail.com>
2025-05-12 15:48:25 +0800

    backend: Finish basic crud logic for employee

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -1,11 +1,56 @@
 var express = require("express");
 var employeeService = require("../services/employeeService");
+const { error } = require("winston");
 var router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
     const employees = await employeeService.getEmployees();
     res.json(employees);
+  } catch (error) {
+    res.status(500).json({ error: error?.message || "Server error" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const employee = await employeeService.getEmployee(req.params.id);
+    if (!employee) {
+      res.status(404).json({
+        error: "Employee not found",
+      });
+    }
+    res.status(200).json(employee);
+  } catch (error) {
+    res.status(500).json({ error: error?.message || "Server error" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const result = await employeeService.deleteEmployee(req.params.id);
+    res.json({ success: result });
+  } catch (error) {
+    res.status(500).json({ error: error?.message || "Server error" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const result = await employeeService.createEmployee(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({ error: error?.message || "Server error" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const result = await employeeService.updateEmployee(
+      req.params.id,
+      req.body
+    );
+    res.status(200).json(result);
   } catch (error) {
     res.status(500).json({ error: error?.message || "Server error" });
   }
--- backend/services/employeeService.js
+++ backend/services/employeeService.js
@@ -1,10 +1,49 @@
 const { pool, transaction } = require("../dbconfig");
 
 async function getEmployees() {
-  const [rows] = await pool.query("SELECT * from employee");
-  return rows;
+  const [result] = await pool.query("SELECT * from employee");
+  return result;
+}
+
+async function getEmployee(id) {
+  const [result] = await pool.query("SELECT * from employee WHERE emp_id = ?", [
+    id,
+  ]);
+  return result[0] || null;
+}
+
+async function deleteEmployee(id) {
+  const [result] = await pool.query("DELETE FROM employee WHERE emp_id = ?", [
+    id,
+  ]);
+  if (result.affectedRows === 0) {
+    throw new Error("Employee not found");
+  }
+  return result.affectedRows > 0;
+}
+async function createEmployee(data) {
+  const [result] = await pool.query("INSERT INTO employee SET ?", [data]);
+  return { id: result?.insertId, ...data };
+}
+
+async function updateEmployee(id, newUpdates) {
+  return transaction(async (connection) => {
+    const [result] = await connection.query(
+      "UPDATE employee SET ? WHERE emp_id = ?",
+      [newUpdates, id]
+    );
+    if (result.affectedRows === 0) {
+      throw new Error("Employee not found");
+    }
+
+    return getEmployee(id);
+  });
 }
 
 module.exports = {
   getEmployees,
+  getEmployee,
+  deleteEmployee,
+  createEmployee,
+  updateEmployee,
 };
